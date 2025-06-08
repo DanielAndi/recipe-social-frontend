ad3c5f96579841b14fe1c5d2c4bfde8231030e7f
DaniellAndi <danielgrijalva374@gmail.com>
2025-06-08 15:45:32 -0700

    chore: scaffold Next.js + TS + Tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold text-emerald-600">
+      Tailwind is working ✔
+    </h1>
+  );
+}
+
+
